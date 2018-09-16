e5857aa0ae8d0bde183d70121a44db81d792d2b7
danieljustino79 <danieljustino79@gmail.com>
2018-09-16 13:01:05 -0300

    test restController

--- src/main/java/com/restful/Application.java
+++ src/main/java/com/restful/Application.java
@@ -1,9 +1,11 @@
 package com.restful;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class Application {
-    public static void main(String[] args){
-        System.out.print("Java 8 ok");
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
     }
-
-
 }
